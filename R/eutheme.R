c95c2fbec02c607eb9de2b8fb42b760ba0c35ff6
Jorge Cimentada <cimentadaj@gmail.com>
2024-01-23 19:05:33 +0100

    Adapts the mockup app to not using shinysurvey and making the survey manually

--- R/eutheme.R
+++ R/eutheme.R
@@ -5,36 +5,49 @@
 #'
 #' @param ... Arguments passed to \code{\link[shiny]{div}} function.
 #'
-#' @return A \code{shiny.semantic::semanticPage} object with custom styling.
+#' @return A \code{shiny::fluidPage} object with custom styling.
 #' @export
-#'
-#' @examples
-#' \dontrun{
-#' library(shiny)
-#' library(shiny.semantic)
-#'
-#' ui <- fluidEutheme(
-#'   tabset(
-#'     tabs = list(
-#'       list(
-#'         menu = "Tab 1",
-#'         content = "hey",
-#'         id = "first_tab"
-#'       )
-#'     )
-#'   )
-#' }
-#' @seealso \code{\link[shiny]{addResourcePath}}, \code{\link[shiny]{includeHTML}}
 fluidEuTheme <- function(...) {
   # Add custom CSS and HTML from inst/www/
   shiny::addResourcePath("custom_css", system.file("www", package = "eutheme"))
   header_html <- shiny::includeHTML(system.file("www/header.html", package = "eutheme"))
   footer_html <- shiny::includeHTML(system.file("www/footer.html", package = "eutheme"))
+  digclass_html <- shiny::includeHTML(system.file("www/digclass_logo.html", package = "eutheme"))
 
-  shiny.semantic::semanticPage(
-    margin = "0px",
-    header_html,
-    shiny::div(class = "content", ...),
-    footer_html
+  shiny::fluidPage(
+    shiny::tags$style(shiny::HTML(
+      "
+      .container-fluid {
+        margin-right: 0px;
+        margin-left: 0px;
+        padding-right: 0px;
+        padding-left: 0px;
+      }
+      .my-body {
+        display: flex;
+        align-items: start; /* Align items to the start (top) of the container */
+        flex-direction: column;
+        width: 95%; /* Default width for mobile */
+        margin: 0 auto; /* Center the container */
+        font-size: 18px; /* Adjust font size here */
+        text-align: left; /* Align text to the left */
+      }
+      /* For tablets (medium screens): min-width 768px */
+      @media (min-width: 768px) {
+        .my-body {
+          width: 75%;
+        }
+      }
+      /* For computer screens (large screens): min-width 992px */
+      @media (min-width: 992px) {
+        .my-body {
+          width: 60%;
+        }
+      }
+  }")),
+      header_html,
+      digclass_html,
+      shiny::div(class = "my-body", ...),
+      footer_html
   )
 }
